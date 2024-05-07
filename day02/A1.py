8c3d75836efd33b38e2d9470736c4485c51f81cb
Shaked <shakedlevy1.61@gmail.com>
2024-05-07 18:23:37 +0300

    running balck

--- day02/A1.py
+++ day02/A1.py
@@ -2,15 +2,16 @@
 # inputs: numbers
 # outputs: larger, lower, or equal and correct. if equal- returns the number of giesses and the time that take
 
-import random 
+import random
 import time
 
 num = random.randrange(1, 21)
 num = 12
 n = 0
 t = 0
-print('\n')
-print("Find the number I choose! (Between 1 to 20) \n\
+print("\n")
+print(
+    "Find the number I choose! (Between 1 to 20) \n\
 \n\
 Your score includes the time t and the number of trials n . For instance: \n\
 n=1, t=1 : score = 100 \n\
@@ -18,38 +19,40 @@ n=3, t=10 : score = 96 \n\
 n=6, t=12 : score = 70 \n\
 n=20, any t : score = 0 \n\
 \n\
-so it's importent to think fast, but luck and strategy are better! \n")
+so it's importent to think fast, but luck and strategy are better! \n"
+)
 
-while True :
-    y_n = input('Press y if you want to start \n\
+while True:
+    y_n = input(
+        "Press y if you want to start \n\
 (pay attention, the clock will start to run!) \n\
-\n')
-    if y_n == 'y' :
+\n"
+    )
+    if y_n == "y":
         start = time.time()
-        while True :
+        while True:
             n = n + 1
             guess = input("Youre guuess: ")
-            if int(guess) == num :
+            if int(guess) == num:
                 t = time.time() - start
-                score = round(100*(1-((n-1)/19))*(1/(1+(0.004*t))))
-                print(f"Congratulations (: , You are correct! \n\
+                score = round(100 * (1 - ((n - 1) / 19)) * (1 / (1 + (0.004 * t))))
+                print(
+                    f"Congratulations (: , You are correct! \n\
 Number of trials: {n} \n\
 Time: {t} second \n\
-Score: {score}")
+Score: {score}"
+                )
                 break
-            elif int(guess) > num :
-                print(f"Sory ): , your guess is biger than the number \n\
-try again")
-            else :
-                print(f"Sory ): , your guess is smaller than the number. \n\
-try again")
+            elif int(guess) > num:
+                print(
+                    f"Sory ): , your guess is biger than the number \n\
+try again"
+                )
+            else:
+                print(
+                    f"Sory ): , your guess is smaller than the number. \n\
+try again"
+                )
         break
-    else :
+    else:
         continue
-
-
-
-        
-
-
-
